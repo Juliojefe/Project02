be9e799be917c92982a7f43660185993ecf54428
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-02-23 15:25:10 -0800

    Initial backend setup

---
+++ src/main/java/com/example/project2_tierlist_backend/Project2TierlistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project2_tierlist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2TierlistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2TierlistBackendApplication.class, args);
+	}
+
+}
