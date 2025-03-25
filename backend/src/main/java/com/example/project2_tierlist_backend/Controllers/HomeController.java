5e67813fd94b258a94ff071c585c42e5acdcf873
OzzieMunoz <osvaldomunoz1205@gmail.com>
2025-03-25 08:14:14 -0700

    Fix home controller imports

--- backend/src/main/java/com/example/project2_tierlist_backend/Controllers/HomeController.java
+++ backend/src/main/java/com/example/project2_tierlist_backend/Controllers/HomeController.java
@@ -1,3 +1,8 @@
+package com.example.project2_tierlist_backend.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 public class HomeController {
     @GetMapping("/")
